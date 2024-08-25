1983d9d096912f20734a60bfd12c12c5c216a32f
Shivam Rajput <raj.shiviraj@gmail.com>
2024-08-25 12:06:27 +0530

    Shivi | update build.sh

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM node:19-alpine
 WORKDIR /app
 ENV NODE_ENV production
 ENV PORT 1337
-COPY dist ./dist
-COPY node_modules ./node_modules
-COPY package.json ./package.json
+COPY . .
 EXPOSE 1337
 CMD ["npm", "start"]
--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-rm -rf node_modules
-npm install && \
+
 cd src/plugins/strapi-plugin-ckeditor && npm install && \
 cd ../../.. && \
-npm run build --omit=dev && \
-rm -rf node_modules && \
-npm install --omit=dev --ignore-scripts --prefer-offline && \
-docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/marketplace-backend:latest --push .
+
+rm -rf node_modules
+npm install --platform=linuxmusl --arch=x64 sharp
+npm run build
+docker buildx build --no-cache --platform=linux/amd64 -t shiviraj/marketplace-backend:latest --push .
+
+rm -rf node_modules
+npm install --platform=linuxmusl --arch=arm64 sharp
+npm run build
+docker buildx build --no-cache --platform=linux/arm64 -t shiviraj/marketplace-backend:arm64 --push .
